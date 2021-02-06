64bd2c89d9a893f938635a2c5706169d3a371c5a
Andreas Maschke <thargor6@googlemail.com>
2021-02-06 19:44:44 +0100

    added login-form from the official Vaadin CRM demo as intermediate solutiuon

--- frontend/routes.ts
+++ frontend/routes.ts
@@ -1,4 +1,6 @@
 import { Flow } from '@vaadin/flow-frontend/Flow';
+import { isLoggedIn, logout } from './auth';
+import { Router, Context, Commands } from '@vaadin/router';
 
 const { serverSideRoutes } = new Flow({
   imports: () => import('../target/frontend/generated-flow-imports'),
@@ -7,10 +9,38 @@ const { serverSideRoutes } = new Flow({
 export const routes = [
   // for client-side, place routes below (more info https://vaadin.com/docs/v18/flow/typescript/creating-routes.html)
   {
-    path: '',
-    component: 'main-view',
+    path: '/login',
+    component: 'login-view',
     action: async () => {
+      await import ('./views/login/login-view');
+    },
+  },
+  // Logging out is handled by Spring Security: it handles HTTP GET requests to
+  // /logout and redirects to /login?logout in response.
+  // For that a "Logout" button should be an regular <a> tag (see main-layout.ts):
+  //    `<a href="/logout" router-ignore>Log out</a>`
+  //
+  // In order to implement logging out on the client-side (e.g. in order to avoid
+  // a full page reload), it would require a `/logout` route like the one below.
+  // In that case a "Logout" button should an in-app link like
+  //    `<a href="/logout">Log out</a>`
+  {
+    path: '/logout',
+    action: async (_: Context, commands: Commands) => {
+      await logout();
+      return commands.redirect('/');
+    }
+  },
+  {
+    path: '/',
+    component: 'main-view',
+
+    action: async (_: Router.Context, commands: Router.Commands) => {
       await import('./views/main/main-view');
+      if (!isLoggedIn()) {
+        return commands.redirect('/login');
+      }
+      return undefined;
     },
     children: [
       {
---
+++ frontend/views/login/login-view.ts
@@ -0,0 +1,101 @@
+import { customElement, html, LitElement, property } from 'lit-element';
+
+import '@vaadin/vaadin-login/vaadin-login-overlay';
+import { LoginI18n } from '@vaadin/vaadin-login';
+import { Router, AfterEnterObserver, RouterLocation } from '@vaadin/router';
+import type { LoginResult } from '@vaadin/flow-frontend';
+import { Lumo } from '../utils/lumo';
+import { login } from '../../auth';
+import styles from './login-view.css';
+
+@customElement('login-view')
+export class LoginView extends LitElement implements AfterEnterObserver {
+
+  @property({type: Boolean})
+  private error = false;
+
+  @property()
+  private errorTitle = '';
+
+  @property()
+  private errorMessage = '';
+
+  private returnUrl = '/';
+
+  private onSuccess: (result: LoginResult) => void;
+
+  static styles = [Lumo, styles];
+
+  constructor(){
+    super();
+    this.onSuccess = () => {
+      Router.go(this.returnUrl);
+    };
+  }
+
+  private static popupResult?: Promise<LoginResult>;
+  static async openAsPopup(): Promise<LoginResult> {
+    if (this.popupResult) {
+      return this.popupResult;
+    }
+
+    const popup = new this();
+    return this.popupResult = new Promise(resolve => {
+      popup.onSuccess = result => {
+        this.popupResult = undefined;
+        popup.remove();
+        resolve(result);
+      }
+      document.body.append(popup);
+    });
+  }
+
+  render() {
+    return html`
+      <vaadin-login-overlay
+        opened 
+        .error=${this.error}
+        .i18n="${this.i18n}"
+        @login="${this.login}">    
+      </vaadin-login-overlay>
+    `;
+  }
+
+  onAfterEnter(location: RouterLocation) {
+    this.returnUrl = location.redirectFrom || this.returnUrl;
+  }
+
+  async login(event: CustomEvent): Promise<LoginResult> {
+    this.error = false;
+    const result = await login(event.detail.username, event.detail.password);
+    this.error = result.error;
+    this.errorTitle = result.errorTitle || this.errorTitle;
+    this.errorMessage = result.errorMessage || this.errorMessage;
+
+    if (!result.error) {
+      this.onSuccess(result);
+    }
+
+    return result;
+  }
+
+  private get i18n(): LoginI18n {
+    return {
+      header: {
+        title: 'Vaadin CRM',
+        description: 'Demo app for the Java Web App tutorial series. Log in with user: user and password: password.'
+      },
+      form: {
+        title: 'Log in',
+        username: 'Username',
+        password: 'Password',
+        submit: 'Log in',
+        forgotPassword: 'Forgot password'
+      },
+      errorMessage: {
+        title: this.errorTitle,
+        message: this.errorMessage
+      },
+    };
+  }
+}
