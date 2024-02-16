c5dbea8c29b3a1134e9623be0c27e13222deebea
thargor6 <thargor6@googlemail.com>
2024-02-16 02:29:57 +0100

    using streamlit-navbar-flaskless by  BugzTheBunny as foundation for the global app layout

---
+++ views/about.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def load_view():
+    st.title('About Page')
+    st.markdown('Uses [streamlit-navbar-flaskless](https://github.com/BugzTheBunny/streamlit-navbar-flaskless) by BugzTheBunny',unsafe_allow_html=True)
